ef529d2c285be2c6dadd79a8837feb079f970c2a
Abderrahim TOUZOURT <a.touzourt@gmail.com>
2025-11-05 11:21:06 +0100

    Install sanctum package

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,22 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+
+Route::middleware('auth:sanctum')->get('/profile', function(Request $request){
     return $request->user();
-})->middleware('auth:sanctum');
+});
+
+Route::post('/login', function(Request $request){
+    $user = User::where('email', $request->email)->firstOrFail();
+    $token = $user->createToken('auth_token')->plainTextToken;
+    
+    return response()->json(["access_token" => $token, "type_token" => "Bearer"]);
+
+});
